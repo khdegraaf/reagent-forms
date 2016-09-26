2a7eb2261c191a644ffad74cf9c920f1e1e8f398
Kevin DeGraaf <khdegraaf@gmail.com>
2016-09-26 12:12:57 -0700

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reagent-forms "0.5.25"
+(defproject reagent-forms "0.5.26"
   :description "data binding library for Reagent"
   :url "https://github.com/yogthos/reagent-forms"
   :license {:name "Eclipse Public License"
